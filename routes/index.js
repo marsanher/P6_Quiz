e4f9f5863d4e03802f5464c87838c8d8e67c4c00
jperezu <36295772+jperezu@users.noreply.github.com>
2018-05-19 13:08:11 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const url = process.env.DATABASE_URL || "sqlite:quizzes.sqlite"
 const quizController = require('../controllers/quiz'); 
 /* GET home page. */
 router.get('/', (req, res, next) => {
